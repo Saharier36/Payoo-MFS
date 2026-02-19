b529ced9607d1997d9e6813187a28461006ce30f
Saharier Omi <gsaharier761@gmail.com>
2026-02-19 15:39:49 +0600

    first commit

---
+++ script/login.js
@@ -0,0 +1,18 @@
+console.log("login");
+document.getElementById("login-btn").addEventListener("click", function () {
+  const numberInput = document.getElementById("input-number");
+  const contactNumber = numberInput.value;
+  console.log(contactNumber);
+
+  const inputPin = document.getElementById("input-pin");
+  const pin = inputPin.value;
+  console.log(pin);
+
+  if (contactNumber == "01615647730" && pin == "1234") {
+    alert("login success");
+    window.location.assign("/home.html")
+  } else {
+    alert("login failed");
+  }
+  return;
+});
